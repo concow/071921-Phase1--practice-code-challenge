37937f91d9ea6e2aa091c8d98f65d9c935abc5f0
Connor Cowan <ccowan003@comcast.net>
2021-07-28 10:58:05 -0700

    Submission for Deadline

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 const dogUrl = 'http://localhost:3000/pups/';
-//const dogBar = document.querySelector('dog-bar')
 
 fetch(dogUrl)
 .then((res) => res.json())
 .then((data) => addPupElements(data))
 
-//Show Info/Click Event
-function heyYall() {
-
-}
 //add pups to DogBar
 function addPupElements(pupArray) {
     const dogBar = document.getElementById('dog-bar');
     
-    pupArray.forEach((pup) => {
-        //console.log(pup);
-        const pupObj = document.createElement('span')
-        pupObj.innerText = pup.name;
-        //pupObj.span = pup;
-        dogBar.append(pupObj);
-        //console.log(pupObj);
-     });
+    pupArray.forEach(addPups);
 }
 function addPups(pup) {
+    const dogBar = document.getElementById('dog-bar');
+    const pupObj = document.createElement('span');
+    pupObj.innerText = pup.name;
+    //pupObj.span = pup;
+    dogBar.append(pupObj);
+    //console.log(pupObj);
+    //pupObj.addEventListener("click", )
+}
+//Show Info/Click Event
+function showInfo(e) {
+    e.preventDefault();
+    const createInfo = document.createElement('dog-info');
+    const info = document.addEventListener("click", (event) => {
+        const pupInfo = document.getElementById("#dog-bar span");
+        console.log(pupInfo);
 
+        
+    })
 }
